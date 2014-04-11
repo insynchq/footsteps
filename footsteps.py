9959fdf89cd41f8cce753fc92be8336275a59b54
Mark Steve Samson <hello@marksteve.com>
2014-04-11 10:49:06 +0800

    Add sign function

--- footsteps.py
+++ footsteps.py
@@ -1,21 +1,35 @@
 import os
 
 import keen
-from flask import abort, Blueprint, Flask, redirect
+from flask import abort, Blueprint, Flask, redirect, url_for
 from itsdangerous import BadSignature, URLSafeSerializer
 
 footsteps = Blueprint('footsteps', __name__)
 serializer = URLSafeSerializer(os.environ['SECRET_KEY'])
+scheme = os.environ.get('URL_SCHEME', 'https')
+
+
+_app = None
+def sign(url, **kwargs):
+  global _app
+  _app = _app or create_app()
+  with _app.test_request_context():
+    payload = dict(**kwargs)
+    serialized = serializer.dumps(payload)
+    return url_for('footsteps.track', serialized=serialized, _external=True,
+                   _scheme=scheme)
+
 
 @footsteps.route('/<serialized>')
 def track(serialized):
   try:
     payload = serializer.loads(serialized)
-    event = payload.pop('event')
+    event = payload.pop('event', 'footsteps')
     keen.add_event(event, payload)
   except BadSignature:
     abort(404)
   return redirect(payload['url'])
+
 
 def create_app():
   app = Flask(__name__)
