10c4e117a43ad1d59580e4a6582f6a55a7ec2f6f
Mark Steve Samson <hello@marksteve.com>
2014-04-11 11:55:16 +0800

    Fix gunicorn config names

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 bind = '0.0.0.0:6000'
 daemon = True
-error_logfile = 'error.log'
-access_logfile = 'access.log'
-pid = 'gunicorn.pid'
+errorlog = './error.log'
+accesslog = './access.log'
+pidfile = './gunicorn.pid'
